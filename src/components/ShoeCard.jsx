ace032fdf01f5b8f7e5e4444d4d81fbbb0287ee6
Jeevan Kumar <jeevankumarhari2002@gmail.com>
2024-02-27 13:21:41 +0530

    Fixed bug in Hero section.

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -1,13 +1,13 @@
-const ShoeCard = ({imgURL, changeBigShoeImage, bigShoeImage}) => {
+const ShoeCard = ({imgURL, changeBigShoeImage,bigShoeImg}) => {
     const handleClick = () => {
-        if(bigShoeImage != imgURL.bigShoe) {
+        if(bigShoeImg != imgURL.bigShoe) {
             changeBigShoeImage(imgURL.bigShoe)
         }
     }
 
   return (
     <div className={`border-2 rounded-xl
-        ${bigShoeImage === imgURL.bigShoe
+        ${bigShoeImg === imgURL.bigShoe
         ? 'border-coral-red'
         : 'border-transparent'
         } cursor-pointer max-sm:flex-1
