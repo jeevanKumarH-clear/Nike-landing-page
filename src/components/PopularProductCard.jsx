1f5ed13bdffdced4ffce49d3172b618101a6a9b6
Jeevan Kumar <jeevankumarhari2002@gmail.com>
2024-02-27 14:09:41 +0530

    Displayed Images in PopularProducts.

---
+++ src/components/PopularProductCard.jsx
@@ -0,0 +1,10 @@
+
+const PopularProductCard = ({imgURL, name}) => {
+  return (
+    <div className="flex flex-1 flex-col w-full max-sm:w-full">
+        <img src={imgURL} alt={name} className="w-[280px] h-[280px]:"/>
+    </div>
+  )
+}
+
+export default PopularProductCard
